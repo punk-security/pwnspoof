4e34e59b74565619e46a2a5ab32cf6eb46e851eb
SimonGurney <simon.gurney@punksecurity.co.uk>
2021-10-04 21:59:12 +0100

    chore: remove some dependencies

--- tests/output.py
+++ tests/output.py
@@ -1,11 +1,10 @@
 import os
-from random import randint
 import sys
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 from session_generator import SessionGenerator
-from models import ActivityPattern, SessionHandler
+from models import SessionHandler
 import apps
 import datetime
 
